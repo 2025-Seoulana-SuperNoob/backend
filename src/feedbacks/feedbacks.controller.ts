efb14eaec99dce9a16aa86cf16b1679373a6d78e
Taehoon Kim <taehoonkim.dev@gmail.com>
2025-04-05 22:00:48 +0900

    fix: change api endpoint

--- src/feedbacks/feedbacks.controller.ts
+++ src/feedbacks/feedbacks.controller.ts
@@ -2,16 +2,16 @@ import { Controller, Get, Post, Body, Param } from "@nestjs/common";
 import { FeedbacksService } from "./feedbacks.service";
 import { Feedback } from "./schemas/feedback.schema";
 
-@Controller("resumes/:id/feedback")
+@Controller("resumes")
 export class FeedbacksController {
   constructor(private readonly feedbacksService: FeedbacksService) {}
 
-  @Get()
+  @Get(":id/feedback")
   async findFeedbacks(@Param("id") id: string): Promise<Feedback[]> {
     return this.feedbacksService.findFeedbacks(id);
   }
 
-  @Post()
+  @Post(":id/feedback")
   async createFeedback(
     @Param("id") id: string,
     @Body("content") content: string,
